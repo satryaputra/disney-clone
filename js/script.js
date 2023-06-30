22f63490b8add8d479de9c5a1d9ced9b9436792c
masputra <muhammadsatrya12@gmail.com>
2023-06-30 09:04:24 +0700

    menambah bagian product

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ let swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     }
 })
+
+// let swiper = new Swiper(".swiper-product", {
+//     slidesPerView: 4,
+//     spaceBetween: 20,
+//     slidesPerGroup: 4,
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     }
+// })
