da91999e43da6c05ecc328df31b5172ec0b71d48
masputra <muhammadsatrya12@gmail.com>
2023-07-02 10:45:26 +0700

    menambahakn navbar hover

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+// navbar-hover
+// shop
+const shop = document.querySelector('.shop');
+const navHoverShop = document.querySelector('.nav-hover-shop');
+
+shop.addEventListener('mouseover', () => {
+    navHoverShop.style.display = 'block';
+})
+
+shop.addEventListener('mouseleave', () => {
+    navHoverShop.style.display = 'none';
+});
+
+// trending
+const trending = document.querySelector('.trending');
+const navHoverTrending = document.querySelector('.nav-hover-trending');
+
+trending.addEventListener('mouseover', () => {
+    navHoverTrending.style.display = 'block';
+})
+
+trending.addEventListener('mouseleave', () => {
+    navHoverTrending.style.display = 'none';
+});
+
+// more
+const more = document.querySelector('.more');
+const navHoverMore = document.querySelector('.nav-hover-more');
+
+more.addEventListener('mouseover', () => {
+    navHoverMore.style.display = 'block';
+})
+
+more.addEventListener('mouseleave', () => {
+    navHoverMore.style.display = 'none';
+});
+
+
 let swiper = new Swiper(".mySwiper", {
     slidesPerView: 5,
     spaceBetween: 20,
